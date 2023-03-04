51ab654a66f25e50c4e917607da87330b49a8f86
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-03-03 20:50:41 -0500

    Added basic API Controller for Users

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.Entities;
+
+namespace API.Controllers
+{
+    public class UsersController : BaseAPIController
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers(){
+            var users = _context.Users.ToList();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,17 @@
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(option =>
+{
+    option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
 // Configure request pipeline
