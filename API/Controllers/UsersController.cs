0ea60de632b250ad641f3db9d88ddd20d6d95e8d
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-03-06 15:26:25 -0500

    Switched user controller get methods to async. Woops.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using API.Data;
 using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -14,15 +15,15 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers(){
-            var users = _context.Users.ToList();
-            return users;
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            return _context.Users.Find(id);
+            return await _context.Users.FindAsync(id);
         }
     }
 }
