d88e6edd4a4b7d138fea5a5e650bb1fab1759a41
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-04-05 14:32:58 -0400

    Migrated front end project from css to sass

--- TODO-UI/src/app/app.component.ts
+++ TODO-UI/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { TodoItemService } from './_services/todo-item.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'ProTask';
--- TODO-UI/src/app/components/todo-item/todo-item.component.ts
+++ TODO-UI/src/app/components/todo-item/todo-item.component.ts
@@ -4,7 +4,7 @@ import { TodoItem } from 'src/app/_models/todo-item';
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent implements OnInit {
 
